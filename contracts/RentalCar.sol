abb981fa91585bb7b47ebb09bc80aa1f45a5f114
Smr0303 <samarmohite7@gmail.com>
2023-08-03 15:46:52 +0530

    Revert "Escrow contract Testing" Done This reverts commit 6a0d070add4c8699633662fc224de851e795bccc.

--- contracts/RentalCar.sol
+++ contracts/RentalCar.sol
@@ -1,35 +1,28 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import "./ERC4907.sol";
 
-contract RentalCar is ERC4907, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract RentalCar is ERC4907 ,Ownable{
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-    event CarTokenCreated(
-        uint256 indexed tokenId,
-        address indexed user,
-        string indexed tokenURI
-    );
+  event CarTokenCreated(uint256 indexed tokenId, address indexed user, string tokenURI);
+    
+  constructor(string memory _name, string memory _symbol) ERC4907 (_name, _symbol){}
+  
+  function createNft(string memory _tokenURI) public returns(uint256 ){
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, _tokenURI);
+    emit CarTokenCreated(newItemId, msg.sender, _tokenURI);
+    return newItemId;
+  }
 
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC4907(_name, _symbol) {}
-
-    function createNft(string memory _tokenURI) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, _tokenURI);
-        emit CarTokenCreated(newItemId, msg.sender, _tokenURI);
-        return newItemId;
-    }
-
-    function getTotalSupply() public view returns (uint256) {
-        return _tokenIds.current();
-    }
+  function getTotalSupply() public view returns (uint256){
+   return _tokenIds.current();
+  }
 }
